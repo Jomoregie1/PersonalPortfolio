95ed81bb98a9092b5b7315eb69e7127fe635c0b4
Joseph <josephomoregie1@outlook.com>
2023-09-24 21:53:21 +0100

    created detail.html,all_projects.html template, added a url and views

--- projects/views.py
+++ projects/views.py
@@ -6,3 +6,8 @@ def all_projects(request):
     projects = Project.objects.all()
     return render(request, template_name='projects/all_projects.html'
                   , context={'projects': projects})
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html', {'project': project})
